ec8d4e594f814a9804b948cecb86be1f6b34aca3
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2023-12-17 23:30:59 +0530

    router added with routes being completed 80 percenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import About from "./page/about/About";
+import Home from "./page/home/Home";
+import Contact from "./page/contact/Contact";
+import Project from "./page/project/Project";
+import Resume from "./page/resume/Resume";
 
 const App = () => {
-  return <h1 className="text-xxl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
