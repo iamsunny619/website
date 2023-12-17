ea8723a35cd2010bfe8c1e022fff99f988af8e2b
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2023-12-18 00:01:29 +0530

    all setup for content and design is ready

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Home from "./page/home/Home";
 import Contact from "./page/contact/Contact";
 import Project from "./page/project/Project";
 import Resume from "./page/resume/Resume";
+import Error from "./page/Error";
+import Footer from "./component/footer/Footer";
+import Navbar from "./component/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
