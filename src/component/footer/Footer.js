f6f9c62308ba1bb955f460de02b952586ed471ee
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2024-01-04 22:59:15 +0530

    some data

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { CiInstagram } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { CiMail } from "react-icons/ci";
+import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container bg-black w-screen relative">
+      <CiLinkedin className="w-10 h-10 m-4 " />
+      <CiMail className="w-10 h-10 m-4 " />
+      <CiInstagram className="w-10 h-10 m-4 " />
+    </div>
+  );
 };
 
 export default Footer;
